f7e181adf07a5fe1650b96324a47c79d573036cc
Eugene Roy <box.rz.box@gmail.com>
2021-06-20 21:27:26 +0300

    routing & forms

--- Express2.0/app.js
+++ Express2.0/app.js
@@ -1,17 +1,23 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware.");
-  next();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/")
+})
+
+app.use("/add-product",(req, res, next) => {
+    // console.log("Add product page.");
+    res.send("<form action='product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>")
 });
 
-app.use((req, res, next) => {
-  console.log("Second middleware.");
-  next();
+app.use("/",(req, res, next) => {
+//   console.log("Home page.");
+  res.send("<h1>Home</h1>");
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
