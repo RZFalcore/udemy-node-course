478ddd9f52a678c97029c18d3f9eb572e8be4115
Eugene Roy <box.rz.box@gmail.com>
2021-06-20 17:22:19 +0300

    middleware

---
+++ Express2.0/app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("First middleware.");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Second middleware.");
+  next();
+});
+
+const server = http.createServer(app);
+server.listen(3000);
