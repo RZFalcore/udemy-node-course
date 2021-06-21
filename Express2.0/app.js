a5c7dafb98a413d344476483f89ec9bf591fe907
Eugene Roy <box.rz.box@gmail.com>
2021-06-21 18:03:51 +0300

    routes, 404

--- Express2.0/app.js
+++ Express2.0/app.js
@@ -1,23 +1,16 @@
 const express = require("express");
 
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+const errorRouter = require("./routes/error");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-app.use("/product", (req, res, next) => {
-    console.log(req.body)
-    res.redirect("/")
-})
-
-app.use("/add-product",(req, res, next) => {
-    // console.log("Add product page.");
-    res.send("<form action='product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>")
-});
-
-app.use("/",(req, res, next) => {
-//   console.log("Home page.");
-  res.send("<h1>Home</h1>");
-});
+app.use(adminRouter);
+app.use(shopRouter);
+app.use(errorRouter);
 
 app.listen(3000);
