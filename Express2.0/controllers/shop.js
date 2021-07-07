16c9553b36b6b150bb82523a4b8b7aec38cb67f5
Eugene Roy <box.rz.box@gmail.com>
2021-07-07 09:32:46 +0300

    added ordrers page

--- Express2.0/controllers/shop.js
+++ Express2.0/controllers/shop.js
@@ -20,6 +20,10 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { docTitle: "Cart", path: "/cart" });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { docTitle: "Orders", path: "/orders" });
+};
+
 exports.getCheckout = (req, res, next) => {
   console.log("Checkout");
   res.redirect("/");
