3b9fdc7587b8c898bec0e2745457300f11bc5e91
Eugene Roy <box.rz.box@gmail.com>
2021-07-26 14:45:30 +0300

    connected db to server

--- Express2.0/app.js
+++ Express2.0/app.js
@@ -6,16 +6,21 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorRoutes = require("./routes/error");
 
+const sequelize = require("./utils/database");
+
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,  "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorRoutes);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(() => app.listen(3000))
+  .catch((e) => console.log(e));
